933999fa22ce55b318a55c14e2ac8634736fa926
Joshua Sinyor <josh.sinyor@gmail.com>
2021-02-09 15:02:51 +0000

    Update javaBuzzSpec.js. Reflects refactored tests.

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -2,16 +2,38 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-        javabuzz = new Javabuzz();
         expect(javabuzz.isDivisibleBy3(3)).toBe(true);
     });
 
-    it('divisible by 3', function() {
-        javabuzz = new Javabuzz();
+    it('not divisible by 3', function() {
         expect(javabuzz.isDivisibleBy3(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+        expect(javabuzz.isDivisibleBy5(5)).toBe(true);
+    });
+
+    it('not divisible by 5', function() {
+        expect(javabuzz.isDivisibleBy5(1)).toBe(false);
+    });
+
+    it('divisible by by 15', function() {
+        expect(javabuzz.isDivisibleBy15(15)).toBe(true);
+    });
+
+    it('not divisible by 15', function() {
+        expect(javabuzz.isDivisibleBy15(3)).toBe(false);
+    });
+
+    it('not divisible by 15', function() {
+        expect(javabuzz.isDivisibleBy15(5)).toBe(false);
     });
 
   });
